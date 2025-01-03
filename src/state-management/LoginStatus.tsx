095344a82d6216698785c336105a7197444db3f6
Ebrahim Mohamed <ebrahim.elmohamed26@gmail.com>
2025-01-03 14:12:12 +0300

    creating a hook to access context

--- src/state-management/LoginStatus.tsx
+++ src/state-management/LoginStatus.tsx
@@ -1,36 +1,53 @@
-import React, {useContext} from 'react';
+import React, { useContext } from "react";
 import AuthContext from "./contexts/authContext";
+import useAuth from "./hooks/useAuth";
 
 const LoginStatus = () => {
-    // const [user, setUser] = useState('');
+  // const [user, setUser] = useState('');
 
-    // const [state, dispatch] = useReducer(authReducer, '');
+  // const [state, dispatch] = useReducer(authReducer, '');
 
+  // const {user, dispatch} = useContext(AuthContext);
+  const { user, dispatch } = useAuth();
 
-    const {user, dispatch} = useContext(AuthContext);
-
-    if (user)
-        return (
-            <ul className="nav">
-                <li className="nav-item">
-                    <a className="nav-link">{user}</a>
-                </li>
-                <li className="nav-item">
-                    <a onClick={() => dispatch({type: 'LOGOUT'})} className="nav-link active" href="#"> Logout</a>
-                </li>
-            </ul>
-        )
+  if (user)
     return (
-        <>
-            <ul className="nav">
-                <li className="nav-item">
-                    <a onClick={() => {
-                        dispatch({type: 'LOGIN', userName: 'ebrahim.elmohamed26@gmail.com'})
-                    }} className="nav-link active" href="#">Login</a>
-                </li>
-            </ul>
-        </>
+      <ul className="nav">
+        <li className="nav-item">
+          <a className="nav-link">{user}</a>
+        </li>
+        <li className="nav-item">
+          <a
+            onClick={() => dispatch({ type: "LOGOUT" })}
+            className="nav-link active"
+            href="#"
+          >
+            {" "}
+            Logout
+          </a>
+        </li>
+      </ul>
     );
+  return (
+    <>
+      <ul className="nav">
+        <li className="nav-item">
+          <a
+            onClick={() => {
+              dispatch({
+                type: "LOGIN",
+                userName: "ebrahim.elmohamed26@gmail.com",
+              });
+            }}
+            className="nav-link active"
+            href="#"
+          >
+            Login
+          </a>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default LoginStatus;
