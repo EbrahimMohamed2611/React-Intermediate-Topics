055c90e140643417577e837427254411d9b9cc0a
Ebrahim Mohamed <ebrahim.elmohamed26@gmail.com>
2025-01-04 15:22:48 +0300

    Preventing Unnecessary Renders with Selectors

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -4,8 +4,11 @@ import TaskContext from "./tasks/tasksContext";
 import useCounterStore from "./counter/store";
 
 const NavBar = () => {
+  console.log("NavBar Rendered");
+
   const { tasks } = useContext(TaskContext);
-  const { counter } = useCounterStore();
+  //   const { counter } = useCounterStore();
+  const counter = useCounterStore((selector) => selector.counter); // use a selector to prevent re-rendering
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light  d-flex justify-content-between">
       <span className="badge text-bg-secondary mx-2">
